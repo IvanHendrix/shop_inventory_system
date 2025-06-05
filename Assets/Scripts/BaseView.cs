9af83277bb5b2a0c41b7859eb4f2ba290632ba0b
iv <iv.hendrix1715@gmail.com>
2025-06-05 22:20:47 +0300

    created base classes for view.moved old base classes up

---
+++ Assets/Scripts/BaseView.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Data;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class BaseView : MonoBehaviour
+    {
+    }
+    
+    public class BaseView<T> : BaseView where T : IViewData
+    {
+        public T Data;
+
+        public void SetContext(T context)
+        {
+            OnContextUpdate(Data);
+        }
+
+        protected virtual void OnContextUpdate(T context) { }
+    }
+}
