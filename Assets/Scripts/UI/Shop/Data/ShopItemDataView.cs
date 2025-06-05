23ded45f83f4bbd2d8d5233790d53588a07ee94c
iv <iv.hendrix1715@gmail.com>
2025-06-05 18:23:23 +0300

    cretaed shop item prefab

---
+++ Assets/Scripts/UI/Shop/Data/ShopItemDataView.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure;
+using UnityEngine;
+
+namespace UI.Shop.Data
+{
+    public class ShopItemDataView : ViewData
+    {
+        public string ItemName;
+        public Sprite Icon;
+        public string Description;
+        public int Price;
+    }
+}
---
+++ Assets/Scripts/UI/Shop/ShopItemUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TMPro;
+using UI.Shop.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Shop
+{
+    public class ShopItemUI : ButtonView<ShopItemDataView>
+    {
+        public event Action<ShopItemDataView> OnClick;
+        
+        [SerializeField] private Image _icon;
+        [SerializeField] private TextMeshProUGUI _nameText;
+        [SerializeField] private TextMeshProUGUI _priceText;
+        [SerializeField] private Button _buyButton;
+
+        private void Start()
+        {
+            _buyButton.onClick.AddListener(OnButtonClick);
+        }
+
+        protected override void OnContextUpdate(ShopItemDataView data)
+        {
+            base.OnContextUpdate(data);
+
+            UpdateButton();
+        }
+
+        private void UpdateButton()
+        {
+            _icon.sprite = Data.Icon;
+            _nameText.text = Data.ItemName;
+            _priceText.text = "$" + Data.Price;
+        }
+
+        private void OnButtonClick()
+        {
+            OnClick?.Invoke(Data);
+        }
+
+        private void OnDestroy()
+        {
+            _buyButton.onClick.RemoveListener(OnButtonClick);
+        }
+    }
+}
