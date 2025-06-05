61dbc0f50f2dd146527b926ce5fdd8f6990a9fba
iv <iv.hendrix1715@gmail.com>
2025-06-05 18:45:58 +0300

    added possibility to send description data for shop view

--- Assets/Scripts/UI/Shop/ShopItemUI.cs
+++ Assets/Scripts/UI/Shop/ShopItemUI.cs
@@ -9,15 +9,18 @@ namespace UI.Shop
     public class ShopItemUI : ButtonView<ShopItemDataView>
     {
         public event Action<ShopItemDataView> OnClick;
+        public event Action<string> OnDescriptionSendClick;
         
         [SerializeField] private Image _icon;
         [SerializeField] private TextMeshProUGUI _nameText;
         [SerializeField] private TextMeshProUGUI _priceText;
         [SerializeField] private Button _buyButton;
+        [SerializeField] private Button _descriptionButton;
 
         private void Start()
         {
             _buyButton.onClick.AddListener(OnButtonClick);
+            _descriptionButton.onClick.AddListener(OnDescriptionSendButtonClick);
         }
 
         protected override void OnContextUpdate(ShopItemDataView data)
@@ -38,10 +41,16 @@ namespace UI.Shop
         {
             OnClick?.Invoke(Data);
         }
+        
+        private void OnDescriptionSendButtonClick()
+        {
+            OnDescriptionSendClick?.Invoke(Data.Description);
+        }
 
         private void OnDestroy()
         {
             _buyButton.onClick.RemoveListener(OnButtonClick);
+            _descriptionButton.onClick.RemoveListener(OnDescriptionSendButtonClick);
         }
     }
 }
