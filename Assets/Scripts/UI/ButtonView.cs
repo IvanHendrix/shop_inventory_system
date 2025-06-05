f7961a84ce7d66ae5ce584e085f871396899df14
iv <iv.hendrix1715@gmail.com>
2025-06-05 18:20:42 +0300

    added base class for data and button this context

---
+++ Assets/Scripts/UI/ButtonView.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure;
+using UnityEngine;
+
+namespace UI
+{
+    public class ButtonView<T> : MonoBehaviour where T : ViewData
+    {
+        public T Data;
+
+        protected virtual void OnContextUpdate(T data)
+        {
+            Data = data;
+        }
+    }
+}
