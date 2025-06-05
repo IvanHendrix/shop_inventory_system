90efcdd63ce4f76127b97b650e431292d2615ab0
iv <iv.hendrix1715@gmail.com>
2025-06-05 22:59:13 +0300

    added view manager and resources manager for views controll

--- Assets/Scripts/ViewManager.cs
+++ Assets/Scripts/ViewManager.cs
@@ -1,27 +1,79 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Infrastructure;
+using ResourcesManager;
+using UnityEngine;
 
-namespace Infrastructure
+public class ViewManager : MonoBehaviour
 {
-    public class ViewManager : MonoBehaviour
-    {
-        [SerializeField] private Transform _canvas;
+    [SerializeField] private Transform _canvas;
         
-        public static ViewManager Instance { get; private set; }
+    public static ViewManager Instance { get; private set; }
+        
+    private Dictionary<string, BaseView> _viewCache;
 
-        private void Awake()
+    public void Initialize()
+    {
+        _viewCache = new Dictionary<string, BaseView>();
+    }
+        
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
         {
-            if (Instance != null && Instance != this)
+            Destroy(gameObject); 
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public BaseView SetView(string viewId)
+    {
+        if (_viewCache.ContainsKey(viewId))
+        {
+            return _viewCache[viewId];
+        }
+
+        var view = CreateView(viewId);
+        if (view != null)
+        {
+            _viewCache.Add(viewId, view);
+        }
+
+        return view;
+    }
+    
+    public void RemoveView(BaseView view)
+    {
+        List<string> viewsToDelete = new List<string>();
+        foreach (var item in _viewCache)
+        {
+            if (item.Value == view)
             {
-                Destroy(gameObject); 
-                return;
+                viewsToDelete.Add(item.Key);
             }
-
-            Instance = this;
         }
 
-        public void RegisterView(Transform view)
+        foreach (var item in viewsToDelete)
         {
-            view.SetParent(_canvas);
+            _viewCache.Remove(item);
         }
+        
+        Destroy(view.gameObject);
+    }
+        
+    private BaseView CreateView(string viewId)
+    {
+        if (!_viewCache.ContainsKey(viewId))
+        {
+            throw new Exception("Can't find view with such id " + viewId);
+        }
+
+        var view = Instantiate(ResourcesCache.GetViewById(viewId), _canvas, false);
+        view.name = viewId;
+        view.gameObject.SetActive(true);
+
+        return view;
     }
 }
