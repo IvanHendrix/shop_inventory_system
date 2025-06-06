7187fbf08567dc5b4c422cd43da3dab888120ecc
iv <iv.hendrix1715@gmail.com>
2025-06-06 13:42:39 +0300

    added items static data

---
+++ Assets/Scripts/StaticData/CurrencyConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace StaticData
+{
+    [CreateAssetMenu(fileName = "CurrencyConfig", menuName = "Game/Currency Config")]
+    public class CurrencyConfig : ScriptableObject
+    {
+        public int startingGold;
+    }
+}
